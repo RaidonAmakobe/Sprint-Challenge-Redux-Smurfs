def57848ee0a08db1b77f6edca7d99ba016bd7c2
Raidon <Raidonmgmt@gmail.com>
2019-05-24 13:34:13 -0400

    import axios

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,7 +2,7 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-
+import axios from "axios";
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
